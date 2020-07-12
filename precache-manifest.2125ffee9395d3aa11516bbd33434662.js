1b64f19087583e42d02a5019c6835de4b6639ce9
AutumnDroid <freybalaguera@gmail.com>
2020-07-12 00:29:20 -0500

    Responsive added

---
+++ precache-manifest.2125ffee9395d3aa11516bbd33434662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e228c3ab831d281b4564e9f78dde17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b714b012fe850f51300a",
+    "url": "/static/js/2.6182c2d5.chunk.js"
+  },
+  {
+    "revision": "446d39ff4af4d7378562e937ff41ce18",
+    "url": "/static/js/2.6182c2d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12844ad81bb61d00ea37",
+    "url": "/static/js/main.81274c10.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd9c5b0f67c9e3c8abcc90b6c9a83111.js"
+  "/precache-manifest.2125ffee9395d3aa11516bbd33434662.js"
 );
 
 self.addEventListener('message', (event) => {
