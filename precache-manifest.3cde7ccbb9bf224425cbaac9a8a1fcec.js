9eb52267c7f09ba34750c0f756759c02eab94803
AutumnDroid <freybalaguera@gmail.com>
2020-07-11 21:26:52 -0500

    commit inicial

---
+++ precache-manifest.3cde7ccbb9bf224425cbaac9a8a1fcec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ff549bfdc1c1776340780cc7ab7d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19fbf126f16c8f3f42d",
+    "url": "/static/js/2.d16b978d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d16b978d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693aa52b6eec252f561b",
+    "url": "/static/js/main.93861dc4.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
